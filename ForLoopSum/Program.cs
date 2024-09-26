a5146d87e5bf267699426a289b38b08f141e1404
Mathias Simm <mathias.simm@mail.ee>
2024-09-26 18:23:13 +0300

    add week4 code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int sum = 0;
+
+
+for (int i = 1; i < 5; i++)
+{                                   // loeb 1-st kümneni
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"current total: {sum}");
+    sum = sum + i; //sum += i;
+
+}
+
+Console.WriteLine($"final total {sum}");
+
+
+//for (int i = 10; i > 0; i--)          // i-- = i - 1, loeb 10-st üheni
+//{
+//    Console.WriteLine($"i = {i}");
+//}
+
+
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//For loop 
+//DRY - principle - Do not repeat yourself
+
+
+for(int i = 0; i < 5; i++) //i++ = i + i on samm nt 0+1=1 1<5 siis 1+1=2 jne kuni 5 //int i =0 on start // i < 5 on tingimus millal tsükkli töö lõppeb, siin 0 kuni 4-ni
+{
+    Console.WriteLine("We do not need no education.");
+}
+
+Console.WriteLine("Have a nice day.");
+//Console.WriteLine("We do not need no education.");
+//Console.WriteLine("We do not need no education."); // halb kood
+//Console.WriteLine("We do not need no education.");
